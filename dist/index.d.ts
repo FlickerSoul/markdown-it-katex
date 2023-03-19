5f8c70aa631705f486d3fb3d1207bb79026935a8
Larry Z <13360148+FlickerSoul@users.noreply.github.com>
2023-03-19 01:34:59 -0700

    chore: fix import

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { KatexOptions } from 'katex';
-import MarkdownIt from "markdown-it";
-type MdKatexOptions = KatexOptions & {
-    enableBareBlocks?: boolean;
-    enableMathBlockInHtml?: boolean;
-    enableMathInlineInHtml?: boolean;
-};
-declare const _default: (md: MarkdownIt, options: MdKatexOptions) => void;
-export = _default;
